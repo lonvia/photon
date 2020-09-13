de7eedb28bcbb23fbe47a976800006691ded6dbb
Sarah Hoffmann <lonvia@denofr.de>
2020-09-13 12:06:33 +0200

    fix incompatible function call in country code lib
    CountryCode.getByCode() has switched from being case-insensitive to
    case-sensitive thus breaking code detection.

--- src/main/java/de/komoot/photon/PhotonDoc.java
+++ src/main/java/de/komoot/photon/PhotonDoc.java
@@ -46,7 +46,7 @@ public class PhotonDoc {
     private String houseNumber;
     private Point centroid;
 
-    public PhotonDoc(long placeId, String osmType, long osmId, String tagKey, String tagValue, Map<String, String> name, String houseNumber, Map<String, String> address, Map<String, String> extratags, Envelope bbox, long parentPlaceId, double importance, CountryCode countryCode, Point centroid, long linkedPlaceId, int rankAddress) {
+    public PhotonDoc(long placeId, String osmType, long osmId, String tagKey, String tagValue, Map<String, String> name, String houseNumber, Map<String, String> address, Map<String, String> extratags, Envelope bbox, long parentPlaceId, double importance, String countryCode, Point centroid, long linkedPlaceId, int rankAddress) {
         String place = extratags != null ? extratags.get("place") : null;
         if (place != null) {
             // take more specific extra tag information
@@ -66,7 +66,7 @@ public class PhotonDoc {
         this.bbox = bbox;
         this.parentPlaceId = parentPlaceId;
         this.importance = importance;
-        this.countryCode = countryCode;
+        this.countryCode = CountryCode.getByCode(countryCode, false);
         this.centroid = centroid;
         this.linkedPlaceId = linkedPlaceId;
         this.rankAddress = rankAddress;
